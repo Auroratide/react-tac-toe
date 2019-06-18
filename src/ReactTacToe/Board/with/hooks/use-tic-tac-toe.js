586f49e75d7554057c4efa84a0061c33bdc4ab82
auroratide <tf.auroratide@gmail.com>
2019-06-18 08:12:21 -0500

    Refactor winning tiles condition for readability

--- src/ReactTacToe/Board/with/hooks/use-tic-tac-toe.js
+++ src/ReactTacToe/Board/with/hooks/use-tic-tac-toe.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 
 const blankBoard = ['', '', '', '', '', '', '', '', ''];
 
+const rows = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+const columns = [[0, 3, 6], [1, 4, 7], [2, 5, 8]];
+const diagonals = [[0, 4, 8], [2, 4, 6]];
+const possibleWinningTileSets = rows.concat(columns).concat(diagonals);
+
 const getWinningTilesForSet = (board, a, b, c) => {
   const winningTiles = [false, false, false, false, false, false, false, false, false];
   if(board[a] === board[b] && board[b] === board[c] && board[a] !== '') {
@@ -26,13 +31,10 @@ export default (initialBoard = blankBoard) => {
     swapTurns();
   };
 
-  const winningTiles =
-    [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-     [0, 3, 6], [1, 4, 7], [2, 5, 8],
-     [0, 4, 8], [2, 4, 6]].reduce((prev, cur) => {
-      const winningTiles = getWinningTilesForSet(board, ...cur);
-      return prev.map((t, i) => t || winningTiles[i]);
-    }, [false, false, false, false, false, false, false, false, false])
+  const winningTiles = possibleWinningTileSets.reduce((prev, set) => {
+    const winningTiles = getWinningTilesForSet(board, ...set);
+    return prev.map((t, i) => t || winningTiles[i]);
+  }, [false, false, false, false, false, false, false, false, false])
     
   return { board, select, winningTiles };
 }
