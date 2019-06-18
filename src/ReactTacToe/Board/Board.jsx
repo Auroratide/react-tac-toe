216708b797375dbe4b8c0b6e9233926322dd6cb7
auroratide <tf.auroratide@gmail.com>
2019-06-18 06:54:59 -0500

    Added prop-types

--- src/ReactTacToe/Board/Board.jsx
+++ src/ReactTacToe/Board/Board.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as Styled from './Board.styles';
 
 const Board = ({ board, select }) =>
@@ -7,5 +8,10 @@ const Board = ({ board, select }) =>
       <Styled.Tile key={i} data-testid={`tile-${i}`} onClick={() => select(i)}>{mark}</Styled.Tile>
     )}
   </Styled.Container>;
+
+Board.propTypes = {
+  board: PropTypes.arrayOf(PropTypes.string),
+  select: PropTypes.func
+};
 
 export default Board;
